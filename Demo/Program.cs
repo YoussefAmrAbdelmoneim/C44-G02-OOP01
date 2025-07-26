8a8383eb0a7a20952676100a0c4b77455a3b65ba
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-26 23:51:39 +0300

    Boxing , unBoxing

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Boxing , unBoxing
+            #region Boxing [Value type to reference type] Safe
+            //int x = 100;
+            //object obj = x;
+            //obj = 12.3;
+            //obj = 'A';
+            //obj = 12.12f;
+            //obj = true;
+            //obj = (object)'B';
+            #endregion
+            #region UnBoxing [reference type to value type] unSafe
+            //object obj = "Aliaa";
+            //obj = 10;
+            //obj = new int[] { 1, 2, 3 };
+            //int x = (int)obj;
+            //Console.WriteLine(x);
+            #endregion
+            #endregion
         }
     }
 }
