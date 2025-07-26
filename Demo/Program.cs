57a10016f2cc2f93b355ccbb52b2356c59cbe9d2
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-07-26 23:59:21 +0300

    Nullable value types

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -22,6 +22,35 @@
             //Console.WriteLine(x);
             #endregion
             #endregion
+            #region Nullable value types
+            //int x = 10;
+            //x = null; //invalid
+            //Nullable<int> Y = 10;
+            //Y = null;
+            //int? z =100;
+            //z = null;
+            //int? a = 10;
+            //int b;
+            //if (a !=null)
+            //{
+            //    b = (int)a;
+            //}
+            //else
+            //{
+            //    b = 0;
+            //}
+            //if (a.HasValue)
+            //{
+            //    b = a.Value;
+            //}
+            //else
+            //{
+            //    b = 0;
+            //}
+            //b=a.HasValue ? a.Value : 0;
+            //b = a ?? 0;
+            //b = a.GetValueOrDefault();
+            #endregion
         }
     }
 }
